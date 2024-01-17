14111c6ec5c4473cb222b22c5500f9bc7e43012e
LouisPino <ljpino95@gmail.com>
2024-01-17 18:15:25 -0500

    can request multiple artists entire discography by spotify ID

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect, render_template, request, session
+from spotify import get_all_artist_songs
+
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        data = get_all_artist_songs(request.form.get("artist_id"))
+        return render_template("index.html", data=data, test_data = [{"test_key": request.form.get("artist_id")}])
+        # return render_template("index.html", test_data = [{"test_key": request.form.get("artist_id")}])
+    elif request.method == "GET":
+        return render_template("index.html", data=[{"test_key": "test value"}])
--- spotify.py
+++ spotify.py
@@ -1,70 +1,78 @@
-import requests
-import time
-import os
-from dotenv import load_dotenv
-load_dotenv()
+def get_all_artist_songs(ids):
+    import requests
+    import time
+    import os
+    from dotenv import load_dotenv
+    load_dotenv()
+    # Your client credentials
+    CLIENT_ID = os.getenv("CLIENT_ID")
+    CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 
-# Your client credentials
-CLIENT_ID = os.getenv("CLIENT_ID")
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+    # Get access token
+    auth_url = 'https://accounts.spotify.com/api/token'
+    auth_response = requests.post(auth_url, {
+        'grant_type': 'client_credentials',
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+    })
+    access_token = auth_response.json()['access_token']
 
 
-# Get access token
-auth_url = 'https://accounts.spotify.com/api/token'
-auth_response = requests.post(auth_url, {
-    'grant_type': 'client_credentials',
-    'client_id': CLIENT_ID,
-    'client_secret': CLIENT_SECRET,
-})
-access_token = auth_response.json()['access_token']
+    # Set up the headers
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
 
 
-# Set up the headers
-headers = {
-    'Authorization': f'Bearer {access_token}',
-}
+    # Artist ID
+    # artist_ids = ['2YZyLoL8N0Wb9xBt1NhZWg', '5j93hwFBNo29RJMsWvtzj8', ]
+    artist_ids = ids.split(", ")
+    all_tracks = []
+    # Get artist's albums
+    for idx, id in enumerate(artist_ids):
+        albums_url = f'https://api.spotify.com/v1/artists/{artist_ids[idx]}/albums'
+        albums_response = requests.get(albums_url, headers=headers)
+        print(albums_response)
+        albums = albums_response.json()['items']
+
+        track_ids = []
+        for album in albums:
+            # Get album's tracks
+            tracks_url = album['href'] + '/tracks'
+            tracks_response = requests.get(tracks_url, headers=headers)
+            tracks = tracks_response.json()['items']
+            
+            for track in tracks:
+                track_ids.append(track['id'])
+
+        def get_track_info(track_id):
+            track_url = f'https://api.spotify.com/v1/tracks/{track_id}'
+            track_response = requests.get(track_url, headers=headers)
+            if track_response.status_code == 200:
+                track_resp = track_response.json()
+                track_name = track_resp['name']
+            else:
+                return
+            track_url = f'https://api.spotify.com/v1/audio-features/{track_id}'
+            track_response = requests.get(track_url, headers=headers)
+            if track_response.status_code == 200:
+                track_resp2 = track_response.json()
+                track_vals = track_resp2
+                return [track_name, track_vals]
+            else:
+                print(f"Failed to fetch track {track_id}: Status code {track_response.status_code}")
+                return None
+        track_data=[]
+        for track_id in track_ids:
+            track_info = get_track_info(track_id)
+            if track_info:
+                track_data.append(track_info)
+                
+            time.sleep(0.01)
+        all_tracks.extend(track_data)
+    return all_tracks
 
 
-# Artist ID
-artist_ids = ['2YZyLoL8N0Wb9xBt1NhZWg', '5j93hwFBNo29RJMsWvtzj8', ]
-
-
-# Get artist's albums
-for idx, id in enumerate(artist_ids):
-    albums_url = f'https://api.spotify.com/v1/artists/{artist_ids[idx]}/albums'
-    albums_response = requests.get(albums_url, headers=headers)
-    albums = albums_response.json()['items']
-
-    track_ids = []
-    for album in albums:
-        # Get album's tracks
-        tracks_url = album['href'] + '/tracks'
-        tracks_response = requests.get(tracks_url, headers=headers)
-        tracks = tracks_response.json()['items']
-        
-        for track in tracks:
-            track_ids.append(track['id'])
-
-    def get_track_info(track_id):
-        track_url = f'https://api.spotify.com/v1/tracks/{track_id}'
-        track_response = requests.get(track_url, headers=headers)
-        if track_response.status_code == 200:
-            track_resp = track_response.json()
-            track_name = track_resp['name']
-        track_url = f'https://api.spotify.com/v1/audio-features/{track_id}'
-        track_response = requests.get(track_url, headers=headers)
-        if track_response.status_code == 200:
-            track_resp2 = track_response.json()
-            track_vals = track_resp2
-            return [track_name, track_vals]
-        else:
-            print(f"Failed to fetch track {track_id}: Status code {track_response.status_code}")
-            return None
-
-    for track_id in track_ids:
-        track_info = get_track_info(track_id)
-        if track_info:
-            print(track_info)
-            print()
-        time.sleep(0.01)
+if __name__ == "__main__":
+    get_all_artist_songs()
