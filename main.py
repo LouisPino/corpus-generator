224e70277488f4568905e53a20f3c0badbf67d3f
LouisPino <ljpino95@gmail.com>
2024-03-12 18:58:27 -0400

    sync

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-from flask import Flask, redirect, render_template, request, session, Response
-from spotify import get_all_artist_songs, download_csv, get_artists
+from flask import Flask, redirect, render_template, request, session, Response, stream_with_context
+from spotify import Tracks, Csv, Artists
 from multidict import MultiDict 
+import time
 
 
 app = Flask(__name__, static_url_path="/static")
+# SSE CONTINUOUS RENDERING USING STREAM_WITH_CONTEXT
+def generate_data():
+    # Simulating a process that fetches and yields data incrementally
+    for i in range(10):  # Example loop to represent data fetching
+        # Fetch data from Spotify API or perform processing here
+        time.sleep(1)
+        yield f"data: {i}\n\n"
+
+@app.route('/stream', methods=["GET"])
+def stream():
+    return Response(stream_with_context(generate_data()), mimetype='text/event-stream')
+
+
+
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        data = get_all_artist_songs(request.form.get("artist_names"), request.form.get("dancey-val"), dancey = True if request.form.get("dancey") == "on" else False, )
+        data = Tracks.get_all_artist_songs(request.form.get("artist_names"), request.form.get("dancey-val"), dancey = True if request.form.get("dancey") == "on" else False, )
         return render_template("index.html", data=data)
     elif request.method == "GET":
         return render_template("index.html")
@@ -16,7 +32,7 @@ def index():
 @app.route("/get-artists", methods=["GET", "POST"])
 def get_artists_route():
     if request.method == "POST":
-        data = get_artists(request.form.get("genre"), True if request.form.get("popularity")=="on" else False, request.form.get("popularity-val"), limit= request.form.get("limit"))
+        data = Artists.get(request.form.get("genre"), True if request.form.get("popularity")=="on" else False, request.form.get("popularity-val"), limit= request.form.get("limit"))
         return render_template("get-artists.html", data=data, names = ', '.join([artist["name"] for artist in data]))
     elif request.method == "GET":
         return render_template("get-artists.html")
@@ -24,7 +40,7 @@ def get_artists_route():
 @app.route("/download-csv", methods=["GET", "POST"])
 def download():
     if request.method == "POST":
-        output = download_csv(request.form["data"])
+        output = Csv.download(request.form["data"])
         return Response(output, 
                     mimetype='text/csv',
                     headers={'Content-Disposition': 'attachment; filename=corpus.csv'})
--- spotify.py
+++ spotify.py
@@ -30,55 +30,60 @@ headers = {
     'Authorization': f'Bearer {access_token}',
 }
 
-
-def get_all_artist_songs(names, dancey_val, dancey=False):
-    # get artist IDs
-    artist_names = names.split(", ")
-    artist_ids = []
-    for name in artist_names:
-        search_url = f'https://api.spotify.com/v1/search?q={name}&type=artist'
-        search_response = requests.get(search_url, headers=headers)
-        artist_ids.append(search_response.json()['artists']['items'][0]['id']) 
-
-
-    # Get artist's albums
-    albums = []
-    for id in artist_ids:
-        albums_url = f'https://api.spotify.com/v1/artists/{id}/albums'
-        albums_response = requests.get(albums_url, headers=headers)
-        albums.extend(albums_response.json()['items'])
+class Tracks:
+    def get_all_artist_songs(names, dancey_val, dancey=False):
+        # get artist IDs
+        artist_ids = Tracks.get_artist_ids(names)
+        albums = Tracks.get_artist_albums(artist_ids)
+        return Tracks.get_album_tracks(albums, dancey_val, dancey)
     
-     
-    # Get album's tracks
-    all_tracks = []
-    track_ids = []
-    for album in albums:
-            tracks_url = album['href'] + '/tracks'
-            tracks_response = requests.get(tracks_url, headers=headers)
-            tracks = tracks_response.json()['items']
-            
-            for track in tracks:
-                track_ids.append(track['id'])
-    
-    track_data=[]
-    for track_id in track_ids:
-            track_info = get_track_info(track_id)
-            if track_info:
-                if dancey:                   
-                    if track_info["danceability"] >= int(dancey_val)/100:
-                        track_data.append(track_info)
-                else:
-                    track_data.append(track_info)
-                     
+    def get_artist_ids(names):
+        artist_names = names.split(", ")
+        artist_ids = []
+        for name in artist_names:
+            search_url = f'https://api.spotify.com/v1/search?q={name}&type=artist'
+            search_response = requests.get(search_url, headers=headers)
+            artist_ids.append(search_response.json()['artists']['items'][0]['id']) 
+        return artist_ids
+        
+    def get_artist_albums(artist_ids):
+        # Get artist's albums
+        albums = []
+        for id in artist_ids:
+            albums_url = f'https://api.spotify.com/v1/artists/{id}/albums'
+            albums_response = requests.get(albums_url, headers=headers)
+            albums.extend(albums_response.json()['items'])
+        return albums
+        
+    def get_album_tracks(albums, dancey_val, dancey=False):    
+        # Get album's tracks
+        all_tracks = []
+        track_ids = []
+        for album in albums:
+                tracks_url = album['href'] + '/tracks'
+                tracks_response = requests.get(tracks_url, headers=headers)
+                tracks = tracks_response.json()['items']
                 
-            time.sleep(0.01)
-    all_tracks.extend(track_data)
-    return all_tracks
+                for track in tracks:
+                    track_ids.append(track['id'])
+        
+        track_data=[]
+        for track_id in track_ids:
+                track_info = Tracks.get_track_info(track_id)
+                if track_info:
+                    if dancey:                   
+                        if track_info["danceability"] >= int(dancey_val)/100:
+                            track_data.append(track_info)
+                    else:
+                        track_data.append(track_info)
+                        
+                    
+                time.sleep(0.01)
+        all_tracks.extend(track_data)
+        return all_tracks
 
-
-
-
-def get_track_info(track_id):
+    def get_track_info(track_id):
+        # get track data
             track_url = f'https://api.spotify.com/v1/tracks/{track_id}'
             track_response = requests.get(track_url, headers=headers)
             if track_response.status_code == 200:
@@ -102,41 +107,41 @@ def get_track_info(track_id):
                 print(f"Failed to fetch track {track_id}: Status code {track_response.status_code}")
                 return None
 
+class Csv:
+    def download(data):
+        output = StringIO()
+        headers = ["title", "artist", "album", "danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instrumentalness", "liveness", "valence", "tempo", "id", "uri", "track_href", "analysis_url", "duration_ms", "time_signature"]
+        writer = csv.DictWriter(output, fieldnames = headers)
+        writer.writeheader()
+        pythonized = ast.literal_eval((data))
+        for track in pythonized:
+            writer.writerow(track)
+        output.seek(0)
+        return output.getvalue()
 
-def download_csv(data):
-     output = StringIO()
-     headers = ["title", "artist", "album", "danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instrumentalness", "liveness", "valence", "tempo", "id", "uri", "track_href", "analysis_url", "duration_ms", "time_signature"]
-     writer = csv.DictWriter(output, fieldnames = headers)
-     writer.writeheader()
-     pythonized = ast.literal_eval((data))
-     for track in pythonized:
-        writer.writerow(track)
-     output.seek(0)
-     return output.getvalue()
+class Artists: 
+    def get(genre, popularity, popularity_val, limit):
+        artists = []
+        offset = 0
+        hit_pop_val = False
+        def request_artists():
+            search_url = f'https://api.spotify.com/v1/search?q=genre%3A%22{genre}%22&type=artist&limit=10&offset={offset}'
+            print(search_url)
+            search_response = requests.get(search_url, headers=headers)
+            print(search_response)
+            for item in search_response.json()['artists']['items']:
+                if item["popularity"] > int(popularity_val):
+                    artists.append({"name": item["name"], "popularity": item["popularity"]})
+                else:
+                        return True
 
-
-def get_artists(genre, popularity, popularity_val, limit):
-    artists = []
-    offset = 0
-    hit_pop_val = False
-    def request_artists():
-        search_url = f'https://api.spotify.com/v1/search?q=genre%3A%22{genre}%22&type=artist&limit=10&offset={offset}'
-        print(search_url)
-        search_response = requests.get(search_url, headers=headers)
-        print(search_response)
-        for item in search_response.json()['artists']['items']:
-            if item["popularity"] > int(popularity_val):
-                artists.append({"name": item["name"], "popularity": item["popularity"]})
-            else:
-                    return True
-
-    if popularity: 
-         while not hit_pop_val:
-            hit_pop_val = request_artists()
-            offset+=10
-    else:           
-        while len(artists) < int(limit):
-            hit_pop_val = request_artists()
-            offset+=10
-    
-    return artists
+        if popularity: 
+            while not hit_pop_val:
+                hit_pop_val = request_artists()
+                offset+=10
+        else:           
+            while len(artists) < int(limit):
+                hit_pop_val = request_artists()
+                offset+=10
+        
+        return artists
